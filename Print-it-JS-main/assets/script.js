d3edba9089854b0624464c129dbf16290c3b2caf
SopMI82 <sompydev@gmail.com>
2024-01-25 17:56:40 +0100

    défilement ok

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -41,12 +41,6 @@ console.log(arrowRight); // vérifier
 let divDots = document.querySelector(".dots")
 console.log(divDots);
 
-<<<<<<< Updated upstream
-//les bullet points :
-const dot = document.querySelectorAll('dot')
-console.log(dot);
-//donne une liste vide à ce niveau car l'objet n'est pas encore créé
-=======
 //récupération des données depuis le tableau pour créer 4 divs contenant chacune une photo et un paragraphe :
 slides.forEach((slide) => {
 	banner.insertAdjacentHTML('beforeEnd',
@@ -56,36 +50,16 @@ slides.forEach((slide) => {
 		<p>${slide.tagLine}</p>
 		</div>` )
 })
->>>>>>> Stashed changes
 
 //le contenu de chaque slide :
 let slideContent = document.querySelectorAll('.slide')
 console.log(slideContent);
-<<<<<<< Updated upstream
-//donne une liste vide à ce niveau car l'objet n'est pas encore créé
-
-//image à actualiser :
-const bannerImg = document.querySelector(".banner-img")
-console.log(bannerImg)
-//pas utile si <img> générée par le JS ?
-
-// paragraphe à modifier :
-const bannerTxt = document.querySelector("p");
-console.log(bannerTxt)
-//pas utile si <p></p> généré par le JS
-=======
->>>>>>> Stashed changes
 
 //Initialisation du compteur :
 let index = 0
 
 //créer les dots, grace à une boucle :
 for(let i=0; i<slides.length; i++) {
-<<<<<<< Updated upstream
-	divDots.insertAdjacentHTML('afterbegin',`<div class="dot"></div>`)
-	//ajouter le dot qui vient d'être crée au tableau, pour l'item[i]
-}
-=======
 	divDots.insertAdjacentHTML('afterBegin',`<div class="dot"></div>`)
 	//ajouter le dot qui vient d'être crée au tableau, pour l'item[i] ?
 }
@@ -104,7 +78,6 @@ slideContent[0].classList.add('slide_selected')
 
 
 //Créer les evenements :
->>>>>>> Stashed changes
 
 // écouter le clic sur la flèche gauche :
 arrowLeft.addEventListener ("click", (left) => {
@@ -133,52 +106,7 @@ arrowRight.addEventListener ("click", (right) => {
 	} else {
 		index = index + 1
 	}
-<<<<<<< Updated upstream
-	console.log(index); // vérifier
-	
-})
-
-//gestion de l'implémentation du contenu à positionner en dehors ?
-
-
-//récupération des données depuis le tableau :
-slides.forEach((slide) => {
-	banner.insertAdjacentHTML('afterbegin', 
-		`<div class="slide">
-		<img class="banner-img" src="./assets/images/slideshow/${slide.image}"
-		alt="Banner Print-it"> <br>
-		<p>${slide.tagLine}</p>
-		</div>` )
-	})
-
-//maintenant toutes mes slides sont positionnées les unes sur les autres, avec un leger décallage.
-//je vais essayer de masquer les slides par défaut dans le code HTML inséré plus tot, pour les afficher uniquement au clic.
-
-
-
-
-	/* const img = `<img class="banner-img" src="./assets/images/slideshow/${slide.image}" alt="Banner Print-it">`
-	const tag = `<p>${slide.tagLine}</p>` */
-/* 	console.log(img); // vérifier
-	console.log(tag); // vérifier */
-
-
-
-
-//insertion des images et des paragraphes dans le code HTML :
-//banner.insertAdjacentHTML("afterbegin", img);
-//banner.insertAdjacentHTML("afterbegin", tag);
-
-//je cherche comment masquer les images et paragraphes qui ne correspondent pas au nuléro de l'index en leur ajoutant la propriété css display none
-/* if (slide != index) {
-		bannerImg.style.display = 'none'
-	}
-}) */
-
-//le mieux serait surement d'afficher uniquement l'image et le paragraphes de la slide correspondant à l'index
-=======
 	console.log(index); // vérifier	
 	dot[index].classList.add('dot_selected')
 	slideContent[index].classList.add('slide_selected')
 })
->>>>>>> Stashed changes
