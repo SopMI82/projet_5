fd094d8b542afcce0e58f745f93c499f05df767c
SopMI82 <sompydev@gmail.com>
2024-01-26 19:58:29 +0100

    refactorisation
    refactorisation du code en cours
    blocage sur dernière fonction

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-/* Données fournies (tableau contenu du carousel) */
+/** TABLEAU DES DONNEES A INTEGRER */
 
 const slides = [
 	{
@@ -19,93 +19,109 @@ const slides = [
 	}
 ]
 
-// SELECTION & STOCKAGE des éléments :
+/** VARIABLES */
 
-//nombre de slides dans mon tableau (= nombre de bullet points) :
-const nbrSlides = slides.length
-console.log(nbrSlides); // vérifier
-
-//bannière (=carousel)
-const banner = document.getElementById("banner")
-console.log(banner); // vérifier
-
-//flèche gauche :
-let arrowLeft = document.querySelector(".arrow_left")
-console.log(arrowLeft); // vérifier
-
-//flèche droite :
-let arrowRight = document.querySelector(".arrow_right")
-console.log(arrowRight); // vérifier
-
-//la div qui doit contenir les bullet points :
-let divDots = document.querySelector(".dots")
-console.log(divDots);
-
-
-//récupération des données depuis le tableau pour créer 4 divs contenant chacune une photo et un paragraphe :
-slides.forEach((slide) => {
-	banner.insertAdjacentHTML('beforeEnd',
-		`<div class="slide">
-		<img class="banner-img" src="./assets/images/slideshow/${slide.image}"
-		alt="Banner Print-it">
-		<p>${slide.tagLine}</p>
-		</div>`)
-})
-
-//le contenu de chaque slide :
-let slideContent = document.querySelectorAll('.slide')
-console.log(slideContent);
-
-//Initialisation du compteur :
 let index = 0
+const nbrSlides = slides.length
+const banner = document.getElementById("banner");
+const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
+const divDots = document.querySelector(".dots");
+const slideContent = document.querySelectorAll('.slide');
+const dot = document.querySelectorAll('.dot');
 
-//créer les dots, grace à une boucle :
-for(let i=0; i<slides.length; i++) {
-divDots.insertAdjacentHTML('afterBegin',`<div class="dot"></div>`)
+/** FONCTIONS */
+
+/**
+ * Fonction qui gère la création des elements du slider
+ * @param {*} 
+ */
+const createDiv = () => {
+	slides.forEach((slide) => {
+		banner.insertAdjacentHTML('beforeEnd',
+			`<div class="slide">
+			<img class="banner-img" src="./assets/images/slideshow/${slide.image}"
+			alt="Banner Print-it">
+			<p>${slide.tagLine}</p>
+			</div>`);
+	})
 }
 
-//selectionner les bullet points :
-const dot = document.querySelectorAll('.dot')
-console.log(dot);
-
-//Initialiser contenu et dots :
-
-//indiquer un index 0 pour le premier bullet point :
-dot[0].classList.add('dot_selected')
-
-//indiquer un index 0 pour la première div contenu :
-slideContent[0].classList.add('slide_selected')
-
-//Créer les evenements :
-
-// écouter le clic sur la flèche gauche :
-arrowLeft.addEventListener ("click", (left) => {
-	console.log("click left OK"); // vérifier
-	// action à effectuer au clic, avec une condition pour faire boucler les slides :
-	dot[index].classList.remove('dot_selected')
-	slideContent[index].classList.remove('slide_selected')
-	if (index<1) {
-		index=slides.length-1
-	} else {
-		index = index -1
-	}	
-	console.log(index); // vérifier
-	dot[index].classList.add('dot_selected')
-	slideContent[index].classList.add('slide_selected')
-})
-
-// écouter le clic sur la flèche gauche :
-arrowRight.addEventListener ("click", (right) => {
-	console.log("click right OK"); // vérifier
-// action à effectuer au clic, avec une condition pour faire boucler les slides :
-	dot[index].classList.remove('dot_selected')
-	slideContent[index].classList.remove('slide_selected')
-	if (index > (slides.length-2)) {
-		index = 0
-	} else {
-		index = index + 1
+/**
+ * Fonction qui gère la création des dots
+ * @param {Number} index 
+ */
+const createDots = (index) => {
+	for(let index=0; index<slides.length; index++) {
+	divDots.insertAdjacentHTML('afterBegin',`<div class="dot"></div>`);
 	}
-	console.log(index); // vérifier	
-	dot[index].classList.add('dot_selected')
-	slideContent[index].classList.add('slide_selected')
-  })
+}
+
+createDiv()
+createDots()
+
+/**
+ * fonction qui incrémente l'index 
+ */
+const augmIndex = () => {//je dois lui passer quoi en paramètre ? clickRight ? ou event ?
+	if (index > (slides.length - 2)) {
+		index = 0;
+	} else {
+		index = index + 1;
+	}
+}
+
+/**
+ * fonction qui décrémente l'index 
+ */
+const dimIndex = () => { //je dois lui passer quoi en paramètre ? clickLeft ? ou event ?
+	if (index < 1) {
+		index = slides.length - 1;
+	} else {
+		index = index - 1;
+	}
+}
+/**
+ * fonction qui enlève les classes au debut du processus
+ * @param {Number} index 
+ */
+const removeClass = (index) => {
+	dot[index].classList.remove('dot_selected');
+	slideContent[index].classList.remove('slide_selected')	;
+}
+
+/**
+ * fonction qui ajoute les classes à la fin du processus
+ * @param {Number} index 
+ */
+const addClass = (index) => {
+	dot[index].classList.add('dot_selected');
+	slideContent[index].classList.add('slide_selected');
+}
+
+
+// a partir de là, je pense que ce que j'ai fait n'est pas bon
+const clickLeft = () => {arrowLeft.addEventListener ("click", (event) => {
+		dimIndex (left);
+		})
+	}
+
+const clickRight = () => {arrowRight.addEventListener ("click", (event) => {
+		addClass(right);
+		})
+	}
+
+const défilerCarousel = () => {
+	removeClass;
+	switch (key) {
+		case value:
+			
+			break;
+	
+		default:
+			break;
+	}
+	//je ne sais plus comment je dois intégrer les fonctions clickLeft et clickRight, je me souviens que tu as utilisé "case"
+	addClass;
+}
+
