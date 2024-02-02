ae9f87fcd4f29e87359a91ae73f961daf343948d
SopMI82 <sompydev@gmail.com>
2024-02-02 15:29:59 +0100

    refactorisation du code
    presque terminé. reste un souci avec l'expression if/else finale, le compteur tourne toujours dans le meme sens

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,21 @@
-/** TABLEAU DES DONNEES A INTEGRER */
+/* Données fournies (tableau contenu du carousel) */
 
 const slides = [
 	{
-		image:"slide1.jpg",
-		tagLine:"Impressions tous formats <span>en boutique et en ligne</span>"
+		image: "slide1.jpg",
+		tagLine: "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		image:"slide2.jpg",
-		tagLine:"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		image: "slide2.jpg",
+		tagLine: "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		image:"slide3.jpg",
-		tagLine:"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		image: "slide3.jpg",
+		tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		image:"slide4.png",
-		tagLine:"Autocollants <span>avec découpe laser sur mesure</span>"
+		image: "slide4.png",
+		tagLine: "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
@@ -24,104 +24,103 @@ const slides = [
 let index = 0
 const nbrSlides = slides.length
 const banner = document.getElementById("banner");
+const arrows = document.querySelectorAll(".arrow");
 const arrowLeft = document.querySelector(".arrow_left");
 const arrowRight = document.querySelector(".arrow_right");
 const divDots = document.querySelector(".dots");
+
+
+//verif
+console.log(index);
+console.log(nbrSlides);
+console.log(banner);
+console.log(arrows);
+console.log(arrowLeft);
+console.log(arrowRight);
+console.log(divDots);
+/* console.log(slideContent);
+console.log(dot); */
+
+
+/**
+ * Création d'une slide pour chaque élément du tableau
+ */
+slides.forEach((slide) => {
+	banner.insertAdjacentHTML('beforeEnd',
+		`<div class="slide">
+		<img class="banner-img" src="./assets/images/slideshow/${slide.image}"
+		alt="Banner Print-it">
+		<p>${slide.tagLine}</p>
+		</div>`)
+})
 const slideContent = document.querySelectorAll('.slide');
+
+/**
+ * Création d'un dot pour chaque élément du tableau
+ */
+for (let i = 0; i < slides.length; i++) {
+	divDots.insertAdjacentHTML('afterBegin', `<div class="dot"></div>`)
+}
 const dot = document.querySelectorAll('.dot');
 
-/** FONCTIONS */
-
 /**
- * Fonction qui gère la création des elements du slider
- * @param {*} 
+ * Fonction qui gère l'ajout de la classe "selected"
  */
-const createDiv = () => {
-	slides.forEach((slide) => {
-		banner.insertAdjacentHTML('beforeEnd',
-			`<div class="slide">
-			<img class="banner-img" src="./assets/images/slideshow/${slide.image}"
-			alt="Banner Print-it">
-			<p>${slide.tagLine}</p>
-			</div>`);
-	})
-}
-
-/**
- * Fonction qui gère la création des dots
- * @param {Number} index 
- */
-const createDots = (index) => {
-	for(let index=0; index<slides.length; index++) {
-	divDots.insertAdjacentHTML('afterBegin',`<div class="dot"></div>`);
-	}
-}
-
-createDiv()
-createDots()
-
-/**
- * fonction qui incrémente l'index 
- */
-const augmIndex = () => {//je dois lui passer quoi en paramètre ? clickRight ? ou event ?
-	if (index > (slides.length - 2)) {
-		index = 0;
-	} else {
-		index = index + 1;
-	}
-}
-
-/**
- * fonction qui décrémente l'index 
- */
-const dimIndex = () => { //je dois lui passer quoi en paramètre ? clickLeft ? ou event ?
-	if (index < 1) {
-		index = slides.length - 1;
-	} else {
-		index = index - 1;
-	}
-}
-/**
- * fonction qui enlève les classes au debut du processus
- * @param {Number} index 
- */
-const removeClass = (index) => {
-	dot[index].classList.remove('dot_selected');
-	slideContent[index].classList.remove('slide_selected')	;
-}
-
-/**
- * fonction qui ajoute les classes à la fin du processus
- * @param {Number} index 
- */
-const addClass = (index) => {
+function addClass() {
 	dot[index].classList.add('dot_selected');
 	slideContent[index].classList.add('slide_selected');
 }
 
-
-// a partir de là, je pense que ce que j'ai fait n'est pas bon
-const clickLeft = () => {arrowLeft.addEventListener ("click", (event) => {
-		dimIndex (left);
-		})
-	}
-
-const clickRight = () => {arrowRight.addEventListener ("click", (event) => {
-		addClass(right);
-		})
-	}
-
-const défilerCarousel = () => {
-	removeClass;
-	switch (key) {
-		case value:
-			
-			break;
-	
-		default:
-			break;
-	}
-	//je ne sais plus comment je dois intégrer les fonctions clickLeft et clickRight, je me souviens que tu as utilisé "case"
-	addClass;
+/**
+ * Fonction qui gère le retrait de la classe "selected"
+ */
+function removeClass() {
+	dot[index].classList.remove('dot_selected');
+	slideContent[index].classList.remove('slide_selected');
 }
+
+/**
+ * Fonction qui retire 1 à l'index
+ */
+function decIndex() {
+	if (index < 1) {
+		index = slides.length - 1
+	} else {
+		index = index - 1
+	}
+}
+
+/**
+ * Fonction qui ajoute 1 à l'index
+ */
+function augIndex() {
+	if (index > (slides.length - 2)) {
+		index = 0
+	} else {
+		index = index + 1
+	}
+}
+
+addClass()
+
+/**
+ * Ecouteur d'évenement qui gère l'affichage du carousel
+ */
+arrows.forEach(arrow=>{
+	arrow.addEventListener('click', event=>{
+		removeClass()
+		if (EventTarget = arrowLeft) {
+			decIndex()
+		} else {
+			augIndex()
+		}
+		addClass()
+	})
+})
+/* 
+tout le reste fonctionne, mais il reste un problème dans l'expression if/else,
+le compteur tourne toujours dans le meme sens (decrementation)
+testé avec =, ==, ===, rien n'y fait
+ */
+
 
